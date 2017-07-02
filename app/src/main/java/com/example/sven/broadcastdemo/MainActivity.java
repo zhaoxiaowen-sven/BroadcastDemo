ce3d256cb92707b7e29d6a8d510f9a8baeee61ac
SVEN <1010773095@qq.com>
2017-07-02 23:20:07 +0800

    Broadcast Begin

--- app/src/main/java/com/example/sven/broadcastdemo/MainActivity.java
+++ app/src/main/java/com/example/sven/broadcastdemo/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.sven.broadcastdemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BroadcastReceiver br = new MyBroadcastReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("MyIntent");
+        registerReceiver(br, intentFilter);
+    }
+
+    class MyBroadcastReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 }
